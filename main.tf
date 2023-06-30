3ba63058b869c03f35b78e2edce5a30e7f375c52
Eloy Gracia <eloygb2000@gmail.com>
2023-06-30 08:40:58 +0200

    Initial files

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-05432c5a0f7b1bfd0" // Amazon Linux 2023
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
