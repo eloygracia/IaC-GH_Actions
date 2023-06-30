c8b19d7e4e0730f31f1f7ac75cff2cf35dfff63d
Eloy Gracia <eloygb2000@gmail.com>
2023-06-30 11:55:22 +0200

    Enhancements

--- main.tf
+++ main.tf
@@ -1,22 +1,25 @@
 provider "aws" {
-  region = "eu-west-1"
+region = "eu-west-1"
 }
 
 data "template_file" "init" {
-  template = file("${path.module}/userdata.sh")
+template = file("${path.module}/userdata.sh")
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-05432c5a0f7b1bfd0" // Amazon Linux 2023
-  instance_type = "t2.micro"
-  
-  user_data = base64encode(data.template_file.init.rendered)
+ami = "ami-05432c5a0f7b1bfd0" // Amazon Linux 2023
+instance_type = "t2.micro"
 
-  tags = {
-    Name = "Terraform IaC GitHub Actions"
-  }
+user_data = base64encode(data.template_file.init.rendered)
+
+vpc_security_group_ids = ["sg-003af02b9a759e866"]
+subnet_id = "subnet-0818b9561675ff47e"
+
+tags = {
+Name = "Terraform IaC GitHub Actions"
+}
 }
 
 output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+value = aws_instance.example.public_ip
 }
