b333c63804d5c9eed731faba867de617bb4b2238
Eloy Gracia <59731892+2000GHz@users.noreply.github.com>
2023-07-11 12:21:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ template = file("${path.module}/userdata.sh")
 resource "aws_instance" "instance-2048" {
   ami                    = var.instance_ami
   instance_type          = var.instance_type
+  user_data = base64encode(data.template_file.init.rendered)
   count                  = 1
   vpc_security_group_ids = ["sg-003af02b9a759e866"]
   key_name               = var.ssh_key
