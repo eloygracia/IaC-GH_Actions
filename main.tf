98a5f0ede5ebbc738e3a9c817171d54a5ca47bf1
Eloy Gracia <59731892+2000GHz@users.noreply.github.com>
2023-07-11 12:11:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,18 +6,16 @@ data "template_file" "init" {
 template = file("${path.module}/userdata.sh")
 }
 
-resource "aws_instance" "example" {
-ami = "ami-05432c5a0f7b1bfd0" // Amazon Linux 2023
-instance_type = "t2.micro"
-
-user_data = base64encode(data.template_file.init.rendered)
-
-vpc_security_group_ids = ["sg-003af02b9a759e866"]
-subnet_id = "subnet-0818b9561675ff47e"
-
-tags = {
-Name = "Terraform IaC GitHub Actions"
-}
+resource "aws_instance" "instance-2048" {
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  count                  = 1
+  vpc_security_group_ids = ["sg-003af02b9a759e866"]
+  key_name               = var.ssh_key
+  tags = {
+    Name = var.instance_name
+    APP  = "vue2048"
+  }
 }
 
 output "instance_public_ip" {
