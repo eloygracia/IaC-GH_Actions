ec59b7ed88d7e13cd078edfd5c48756d3d43776a
Eloy Gracia <59731892+2000GHz@users.noreply.github.com>
2023-07-11 12:09:46 +0200

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,23 @@
+variable "instance_name" {
+  description = "Instance Name"
+  type        = string
+  default     = "infra-production"
+}
+
+variable "ssh_key" {
+  description = "SSH-key name"
+  type        = string
+  default     = "github-actions"
+}
+
+variable "instance_ami" {
+  description = "Instance AMI"
+  type        = string
+  default     = "ami-05432c5a0f7b1bfd0" // Amazon Linux 2023
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+  default     = "t2.micro"
+}
